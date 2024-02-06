0b04f7e3594ca8d344ebaf18efea4161077b2772
Jonathas Magalhães <jondejotam@gmail.com>
2024-02-05 21:25:27 -0300

    Update JobController.kt

--- src/main/java/backend/vagas/controller/JobController.kt
+++ src/main/java/backend/vagas/controller/JobController.kt
@@ -2,17 +2,21 @@ package backend.vagas.controller
 
 import backend.vagas.model.Job
 import backend.vagas.service.JobService
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/job")
 class JobController(val jobService: JobService) {
-    fun create(@Valid @RequestBody job: Job): ResponseEntity<Job> {
+    fun create(@Valid @RequestBody job: Job, request: HttpServletRequest): ResponseEntity<Job> {
+        request.getAttribute("company_id")?.let {
+            job.companyId = it as UUID
+        }
         return this.jobService.createJob(job)
     }
-
 }
